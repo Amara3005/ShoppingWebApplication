79c490de108c9d9cb2a6772c68522ac9c7406ce0
Amara3005 <amaraprasana@gmail.com>
2023-02-07 20:05:30 +0530

    Developed controler Class

--- src/main/java/com/ShopTry/ShoppingWebApplication/ControllerAdmin.java
+++ src/main/java/com/ShopTry/ShoppingWebApplication/ControllerAdmin.java
@@ -10,8 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.shoppingCart.ShopTry.Orders;
-import com.shoppingCart.ShopTry.Product;
+
 
 
 @Controller
