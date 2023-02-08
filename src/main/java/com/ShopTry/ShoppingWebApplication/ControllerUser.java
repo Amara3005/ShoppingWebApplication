a282f0443957d70b99f0442eae2747bd5d70a159
Amara3005 <amaraprasana@gmail.com>
2023-02-08 19:11:42 +0530

    Developed some Code

--- src/main/java/com/ShopTry/ShoppingWebApplication/ControllerUser.java
+++ src/main/java/com/ShopTry/ShoppingWebApplication/ControllerUser.java
@@ -1,5 +1,26 @@
 package com.ShopTry.ShoppingWebApplication;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class ControllerUser {
 
+	@Autowired
+	DaoProduct data;
+	
+	@Autowired
+	DaoOrder orddata;
+	
+	@GetMapping("/products")
+	public String productsAll(Model mod) {
+		List<Product> list = data.allProducts();
+		mod.addAttribute("list", list);
+		return "UserHt";
+	}
 }
