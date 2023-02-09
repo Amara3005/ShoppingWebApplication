1496e18c654de58ec56fe83048307825730f889f
Amara3005 <amaraprasana@gmail.com>
2023-02-09 19:16:33 +0530

    Data Base code

--- src/main/java/com/ShopTry/ShoppingWebApplication/DaoOrder.java
+++ src/main/java/com/ShopTry/ShoppingWebApplication/DaoOrder.java
@@ -1,5 +1,20 @@
 package com.ShopTry.ShoppingWebApplication;
 
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+
+@Repository
+@Transactional
 public class DaoOrder {
 
+	@PersistenceContext
+	EntityManager enty;
+	
+	public void addOrder(Orders ordr) {
+		enty.persist(ordr);
+	}
 }
