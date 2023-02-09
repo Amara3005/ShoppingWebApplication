96539ed0e038b91f054598bc5a8ca4279cab43f6
Amara3005 <amaraprasana@gmail.com>
2023-02-09 19:18:27 +0530

    Data Base Code

--- src/main/java/com/ShopTry/ShoppingWebApplication/DaoProduct.java
+++ src/main/java/com/ShopTry/ShoppingWebApplication/DaoProduct.java
@@ -1,5 +1,24 @@
 package com.ShopTry.ShoppingWebApplication;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+
+@Repository
+@Transactional
 public class DaoProduct {
 
+	@PersistenceContext
+	EntityManager enty;
+	
+	public void addNewProduct(Product prdt) {
+		enty.persist(prdt);
+	}
+	
+	public Product getProduct(int id) {
+		return enty.find(Product.class, id);
+	}
 }
