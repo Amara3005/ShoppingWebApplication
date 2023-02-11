9174e2a0a6fce962ae01f8f8455a7e2e14cf4ad8
Amara3005 <amaraprasana@gmail.com>
2023-02-11 21:41:44 +0530

    Made some work on Backend

--- src/main/java/com/ShopTry/ShoppingWebApplication/Orders.java
+++ src/main/java/com/ShopTry/ShoppingWebApplication/Orders.java
@@ -13,4 +13,44 @@ public class Orders {
 	private Product product;
 	private int qntyNeed;
 	private String adrs;
+	
+
+	public Orders(long id, Product prdtId, int qntyNeed, String adrs) {
+		super();
+		this.id = id;
+		this.product = prdtId;
+		this.qntyNeed = qntyNeed;
+		this.adrs = adrs;
+	}
+	
+	public Orders() {
+		
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+    
+	public int getQntyNeed() {
+		return qntyNeed;
+	}
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public void setQntyNeed(int qntyNeed) {
+		this.qntyNeed = qntyNeed;
+	}
+	public String getAdrs() {
+		return adrs;
+	}
+	public void setAdrs(String adrs) {
+		this.adrs = adrs;
+	}
 }
