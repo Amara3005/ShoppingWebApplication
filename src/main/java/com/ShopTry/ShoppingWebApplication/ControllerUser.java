810bd1d817202cdb77b8b21f7fff04ee2137dc28
Amara3005 <amaraprasana@gmail.com>
2023-02-11 21:42:19 +0530

    Made some work on Backend

--- src/main/java/com/ShopTry/ShoppingWebApplication/ControllerUser.java
+++ src/main/java/com/ShopTry/ShoppingWebApplication/ControllerUser.java
@@ -12,10 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.shoppingCart.ShopTry.Orders;
-import com.shoppingCart.ShopTry.Product;
-
-
 @Controller
 public class ControllerUser {
 
